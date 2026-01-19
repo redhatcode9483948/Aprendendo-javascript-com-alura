2df0cec376f0ab19ee73a568da9d055730a3d194
Pixel_Pioneers <lucasraphael0706@gmail.com>
2026-01-19 12:31:49 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+//mensagem na tela  
+alert('Boas vindas ao jogo do número secreto!');
+//variavel que guarda o número
+let numeroSecreto = 5;
+let chute = prompt ('Escolha um número de 0 até 10:');
+
+//condição para as variaveis
+if (chute == numeroSecreto) {
+alert(`Parabéns! Você acertou encheio, o número secreto é ${numeroSecreto}`);
+console.log(numeroSecreto);
+}else{
+    alert('Você errou :(');
+}
