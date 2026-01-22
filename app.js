21738ccbc190bbb152ce42e6a102b83541068168
Pixel_Pioneers <lucasraphael0706@gmail.com>
2026-01-22 15:39:50 -0300

    Enhance number guessing game with detailed alerts
    Refactor game logic and improve user feedback.

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
 
+
 //mensagem na tela  
-alert('Boas vindas ao jogo do número secreto!');
-//variavel que guarda o número
+alert('Boas-vindas ao jogo do número secreto!');
+//variável que guarda o número
 let numeroSecreto = 5;
-let chute = prompt ('Escolha um número de 0 até 10:');
+let chute = prompt ('Escolha um número de 0 a 10:');
 
 //condição para as variaveis
 if (chute == numeroSecreto) {
 alert(`Parabéns! Você acertou encheio, o número secreto é ${numeroSecreto}`);
-console.log(numeroSecreto);
+console.log('resultado da comparação', chute == numeroSecreto);
+    //condição que verifica se o número é maior ou menor que o "chute"
 }else{
-    alert('Você errou :(');
+   if(chute > numeroSecreto){
+    alert(`O número secreto é menor do que ${chute}`)
+   }else{
+    alert(`O número secreto é maior do que ${chute}`)
+   }
 }
